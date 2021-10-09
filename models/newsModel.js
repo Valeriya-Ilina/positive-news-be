980fc4312ff85712a96e4b0be28853fa12716366
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-10-09 17:36:53 -0400

    create news model

---
+++ models/newsModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsSchema = Schema ({
+  title: {type: String, unique: true, required: true},
+  author: {type: String},
+  url: {type: String},
+  description: {type: String},
+  urlToImage: {type: String},
+  publishedAt: {type: Date}
+}, { timestamps: true });
+
+//creating collection/model
+const News = mongoose.model('News', newsSchema);
+
+module.exports = News;
